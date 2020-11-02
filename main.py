5e4937da7f5354d88966cabb3f1245b62201251c
Lovegood <>
2020-11-02 05:21:05 -0800

    Basic working welcome bot.

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+
+from discord.ext import commands
+from secrets import BOT_TOKEN
+
+
+COMMAND_PREFIX = '!cb '
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Successfully logged in as: {bot.user}')
+    bot.load_extension('cogs.welcome')
+
+
+if __name__ == '__main__':
+    print('Logging in...')
+    bot.run(BOT_TOKEN)
