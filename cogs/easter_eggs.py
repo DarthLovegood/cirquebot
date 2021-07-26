1f9d4023dc9cf29e23750f77cac9033c60f7ac78
Lovegood <>
2021-07-26 14:34:21 -0700

    Add some easter eggs for playing youtube audio.

--- cogs/easter_eggs.py
+++ cogs/easter_eggs.py
@@ -1,9 +1,13 @@
+import asyncio
 import re
 from io import BytesIO
 
 from PIL import Image
-from discord import File
+from discord import File, FFmpegPCMAudio
 from discord.ext import commands
+from youtube_dl import YoutubeDL
+
+from lib.embeds import EMOJI_ERROR, EMOJI_SUCCESS, create_basic_embed
 
 FILENAME_BONK = 'assets/bonk.png'
 FILENAME_MASK = 'assets/mask.png'
@@ -12,9 +16,29 @@ FILENAME_SPANK_1 = 'assets/spank1.png'
 FILENAME_SPANK_2 = 'assets/spank2.png'
 FILENAME_THERON = 'assets/theron.png'
 
+REGEX_BABY_SHARK = re.compile(r'^\s*/babyshark(\s*<@!?[0-9]*>)*\s*$', re.IGNORECASE)
+REGEX_BING_BANG_BONG = re.compile(r'^\s*/bingbangbong(\s*<@!?[0-9]*>)*\s*$', re.IGNORECASE)
 REGEX_BONK = re.compile(r'^\s*/(sunder)?bonk(\s*<@!?[0-9]*>)*\s*$', re.IGNORECASE)
+REGEX_NYAN_CAT = re.compile(r'^\s*/nyancat(\s*<@!?[0-9]*>)*\s*$', re.IGNORECASE)
+REGEX_RICK_ROLL = re.compile(r'^\s*/rickroll(\s*<@!?[0-9]*>)*\s*$', re.IGNORECASE)
 REGEX_SPANK = re.compile(r'^\s*/spank((\s*theron)|(\s*<@!?[0-9]*>)*)\s*$', re.IGNORECASE)
 REGEX_SPANK_EMOJI = re.compile(r'^\s*(<:spank[a-z]*:740455662856831007>\s*)+$', re.IGNORECASE)
+REGEX_STOP_AUDIO = re.compile(r'^\s*/stopaudio\s*$', re.IGNORECASE)
+
+TEXT_BABY_SHARK = '🦈 \u200B \u200B Now playing **Baby Shark** in **{0}**!'
+TEXT_BING_BANG_BONG = '💃 \u200B \u200B Now playing **UK Hun?** in **{0}**!'
+TEXT_NYAN_CAT = '🐱 \u200B \u200B Now playing **Nyan Cat** in **{0}**!'
+TEXT_RICK_ROLL = '🕺 \u200B \u200B Now playing **Never Gonna Give You Up** in **{0}**!'
+
+URL_BABY_SHARK = 'https://www.youtube.com/watch?v=LBHYhvOHgvc'
+URL_BING_BANG_BONG = 'https://www.youtube.com/watch?v=z9wRiNzM6Ww'
+URL_NYAN_CAT = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+URL_RICK_ROLL = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+YTDL_OPTIONS = {
+    'format': 'bestaudio/best',
+    'quiet': True,
+}
 
 
 class EasterEggs(commands.Cog):
@@ -25,9 +49,21 @@ class EasterEggs(commands.Cog):
     async def on_message(self, message):
         if message.author.id == self.bot.user.id:
             return
+        elif REGEX_BABY_SHARK.match(message.content) and len(message.mentions) == 1:
+            await EasterEggs.play_youtube_audio(
+                self.bot, message.mentions[0], message.channel, URL_BABY_SHARK, TEXT_BABY_SHARK)
+        elif REGEX_BING_BANG_BONG.match(message.content) and len(message.mentions) == 1:
+            await EasterEggs.play_youtube_audio(
+                self.bot, message.mentions[0], message.channel, URL_BING_BANG_BONG, TEXT_BING_BANG_BONG)
         elif REGEX_BONK.match(message.content) and len(message.mentions) == 1:
             sunder = 'sunder' in message.content.lower()
             await EasterEggs.bonk(message.channel, message.author, message.mentions[0], sunder=sunder)
+        elif REGEX_NYAN_CAT.match(message.content) and len(message.mentions) == 1:
+            await EasterEggs.play_youtube_audio(
+                self.bot, message.mentions[0], message.channel, URL_NYAN_CAT, TEXT_NYAN_CAT)
+        elif REGEX_RICK_ROLL.match(message.content) and len(message.mentions) == 1:
+            await EasterEggs.play_youtube_audio(
+                self.bot, message.mentions[0], message.channel, URL_RICK_ROLL, TEXT_RICK_ROLL)
         elif REGEX_SPANK.match(message.content):
             if 'theron' in message.content.lower() and len(message.mentions) == 0:
                 await EasterEggs.spank(message.channel, message.author, None, True)
@@ -35,6 +71,28 @@ class EasterEggs(commands.Cog):
                 await EasterEggs.spank(message.channel, message.author, message.mentions[0])
         elif REGEX_SPANK_EMOJI.match(message.content):
             await EasterEggs.spank(message.channel, self.bot.user, message.author)
+        elif REGEX_STOP_AUDIO.match(message.content):
+            await EasterEggs.stop_audio(self.bot, message.channel)
+
+    @staticmethod
+    async def play_youtube_audio(bot, target_user, text_channel, url, confirmation_text):
+        # noinspection PyBroadException
+        try:
+            loop = bot.loop or asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: YoutubeDL(YTDL_OPTIONS).extract_info(url, download=False))
+            audio_source = FFmpegPCMAudio(data['url'])
+            voice_client = await target_user.voice.channel.connect()
+            voice_client.play(audio_source)
+            await text_channel.send(embed=create_basic_embed(confirmation_text.format(target_user.voice.channel.name)))
+        except Exception as exception:
+            print(exception)
+            await text_channel.send(embed=create_basic_embed('Error playing YouTube audio.', EMOJI_ERROR))
+
+    @staticmethod
+    async def stop_audio(bot, text_channel):
+        for voice_client in bot.voice_clients:
+            await voice_client.disconnect()
+        await text_channel.send(embed=create_basic_embed('Successfully stopped all audio playback.', EMOJI_SUCCESS))
 
     @staticmethod
     async def bonk(channel, bonker, bonkee, sunder=False):
