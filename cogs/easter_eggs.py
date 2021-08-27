8992ef57e4c8944447b0769beda39070112ba87d
Lovegood <>
2021-08-27 14:09:05 -0700

    Clean up easter eggs cog, especially bonks and spanks.

--- cogs/easter_eggs.py
+++ cogs/easter_eggs.py
@@ -1,51 +1,66 @@
-import asyncio
 import re
 from PIL import Image
 from discord import File
 from discord.ext import commands
 from io import BytesIO
-from lib.embeds import EMOJI_ERROR, EMOJI_SUCCESS, create_basic_embed
+from lib.embeds import create_basic_embed, EMOJI_ERROR
 
 FILENAME_BONK = 'assets/bonk.png'
 FILENAME_MASK = 'assets/mask.png'
 FILENAME_SUNDER = 'assets/sunder.png'
 FILENAME_SPANK_1 = 'assets/spank1.png'
 FILENAME_SPANK_2 = 'assets/spank2.png'
-FILENAME_THERON = 'assets/theron.png'
 
-REGEX_BONK = re.compile(r'^\s*/(sunder)?bonk(\s*<@!?[0-9]*>)*\s*$', re.IGNORECASE)
 REGEX_QWEPHESS = re.compile(r'^(.*?(\bkephess\b)[^$]*)$', re.IGNORECASE)
-REGEX_SPANK = re.compile(r'^\s*/spank((\s*theron)|(\s*<@!?[0-9]*>)*)\s*$', re.IGNORECASE)
 REGEX_SPANK_EMOJI = re.compile(r'^\s*(<:spank[a-z]*:740455662856831007>\s*)+$', re.IGNORECASE)
 
 
 class EasterEggs(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def bonk(self, ctx):
+        await EasterEggs.handle_bonk_command(ctx.message)
+
+    @commands.command()
+    async def sunderbonk(self, ctx):
+        await EasterEggs.handle_bonk_command(ctx.message, sunder=True)
+
+    @commands.command()
+    async def spank(self, ctx):
+        await EasterEggs.handle_spank_command(ctx.message)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.id == self.bot.user.id:
             return
-        elif REGEX_BONK.match(message.content) and len(message.mentions) == 1:
-            sunder = 'sunder' in message.content.lower()
-            await EasterEggs.bonk(message.channel, message.author, message.mentions[0], sunder=sunder)
         elif REGEX_QWEPHESS.match(message.content):
-            await EasterEggs.fix_qwephess(message.channel, message)
-        elif REGEX_SPANK.match(message.content):
-            if 'theron' in message.content.lower() and len(message.mentions) == 0:
-                await EasterEggs.spank(message.channel, message.author, None, True)
-            elif len(message.mentions) == 1:
-                await EasterEggs.spank(message.channel, message.author, message.mentions[0])
+            await EasterEggs.fix_qwephess(message)
         elif REGEX_SPANK_EMOJI.match(message.content):
-            await EasterEggs.spank(message.channel, self.bot.user, message.author)
+            await EasterEggs.handle_spank_command(message, bot=self.bot)
 
     @staticmethod
-    async def bonk(channel, bonker, bonkee, sunder=False):
-        async with channel.typing():
+    async def fix_qwephess(message):
+        message_blocks = re.split("kephess", message.content, flags=re.IGNORECASE)
+        kephess_index = message.content.lower().find("kephess")
+        kephess_string = message.content[kephess_index:(kephess_index + len("kephess"))]
+        qw = "Qw" if kephess_string[0].isupper() else "qw"
+        new_message = message_blocks[0] + f"~~{kephess_string}~~ {qw}{kephess_string[1:]}" + message_blocks[1]
+        await message.reply(new_message, mention_author=False)
+
+    @staticmethod
+    async def handle_bonk_command(message, sunder=False):
+        if len(message.mentions) != 1:
+            embed = create_basic_embed('Please specify exactly one person to bonk.', EMOJI_ERROR)
+            await message.channel.send(embed=embed)
+            return
+
+        async with message.channel.typing():
             bonk_image = Image.open(FILENAME_BONK)
-            bonker_image = Image.open(FILENAME_SUNDER) if sunder else await EasterEggs.get_avatar_image(bonker)
-            bonkee_image = await EasterEggs.get_avatar_image(bonkee)
+            bonker_image = Image.open(FILENAME_SUNDER) if sunder else await EasterEggs.get_avatar_image(message.author)
+            bonkee_image = await EasterEggs.get_avatar_image(message.mentions[0])
 
             if bonker_image:
                 bonk_image = EasterEggs.process_image(
@@ -59,15 +74,20 @@ class EasterEggs(commands.Cog):
         with BytesIO() as image_bytes:
             bonk_image.save(image_bytes, 'png')
             image_bytes.seek(0)
-            await channel.send(file=File(fp=image_bytes, filename='bonk.png'))
+            await message.channel.send(file=File(fp=image_bytes, filename='bonk.png'))
 
     @staticmethod
-    async def spank(channel, spanker, spankee, theron=False):
-        async with channel.typing():
+    async def handle_spank_command(message, bot=None):
+        if len(message.mentions) != 1 and not bot:
+            embed = create_basic_embed('Please specify exactly one person to spank.', EMOJI_ERROR)
+            await message.channel.send(embed=embed)
+            return
+
+        async with message.channel.typing():
             spank_image_1 = Image.open(FILENAME_SPANK_1)
             spank_image_2 = Image.open(FILENAME_SPANK_2)
-            spanker_image = await EasterEggs.get_avatar_image(spanker)
-            spankee_image = Image.open(FILENAME_THERON) if theron else await EasterEggs.get_avatar_image(spankee)
+            spanker_image = await EasterEggs.get_avatar_image(message.author if not bot else bot.user)
+            spankee_image = await EasterEggs.get_avatar_image(message.mentions[0] if not bot else message.author)
 
             if spanker_image:
                 spank_image_1 = EasterEggs.process_image(
@@ -84,17 +104,7 @@ class EasterEggs(commands.Cog):
         with BytesIO() as image_bytes:
             spank_image_1.save(image_bytes, 'gif', save_all=True, append_images=[spank_image_2], duration=180, loop=0)
             image_bytes.seek(0)
-            await channel.send(file=File(fp=image_bytes, filename='spank.gif'))
-
-    @staticmethod
-    async def fix_qwephess(channel, message):
-        async with channel.typing():
-            message_blocks = re.split("kephess", message.content, flags=re.IGNORECASE)
-            kephess_index = message.content.lower().find("kephess")
-            kephess_string = message.content[kephess_index:(kephess_index + len("kephess"))]
-            qw = "Qw" if kephess_string[0].isupper() else "qw"
-            new_message = message_blocks[0] + f"~~{kephess_string}~~ {qw}{kephess_string[1:]}" + message_blocks[1]
-            await message.reply(new_message, mention_author=False)
+            await message.channel.send(file=File(fp=image_bytes, filename='spank.gif'))
 
     @staticmethod
     async def get_avatar_image(user):
