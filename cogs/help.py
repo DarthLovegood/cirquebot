11f1ca0dd25f04b00b17fd0793f8ac201602bd69
Lovegood <>
2021-08-27 16:44:10 -0700

    Use deepcopy to eliminate duplicates from the help dict.

--- cogs/help.py
+++ cogs/help.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from discord.ext import commands
 from lib.embeds import *
 from lib.prefixes import get_prefix
@@ -29,7 +30,7 @@ class Help(commands.Cog):
 
     @staticmethod
     def build_help_dict(help_cogs: list):
-        help_dict = BASE_HELP_DICT.copy()
+        help_dict = deepcopy(BASE_HELP_DICT)
         for cog_name, cog_object in help_cogs:
             help_dict[KEY_SUBCOMMANDS].append({
                 KEY_EMOJI: cog_object.help[KEY_EMOJI],
