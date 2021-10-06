3ff5c781aa4ebacc92510458f3178add4e21b3ce
Lovegood <>
2021-10-06 15:15:03 -0700

    Add ability to snipe one image attachment.

--- cogs/sniper.py
+++ cogs/sniper.py
@@ -8,6 +8,7 @@ KEY_TIMESTAMP = 'timestamp'  # type: datetime
 KEY_CHANNEL_ID = 'channel_id'  # type: int
 KEY_AUTHOR = 'author'  # type: Member
 KEY_MESSAGE_TEXT = 'message_text'  # type: str
+KEY_ATTACHMENTS = 'attachments'  # type: list[Attachment]
 
 SNIPE_WINDOW = timedelta(seconds=30)
 
@@ -49,13 +50,15 @@ class Sniper(commands.Cog):
                 KEY_TIMESTAMP: timestamp,
                 KEY_CHANNEL_ID: message.channel.id,
                 KEY_AUTHOR: message.author,
-                KEY_MESSAGE_TEXT: message.content
+                KEY_MESSAGE_TEXT: message.content,
+                KEY_ATTACHMENTS: message.attachments
             })
 
     @staticmethod
     async def attempt_snipe(channel, message_cache, message_cache_lock):
         sniped_user = None
         sniped_messages = []
+        sniped_attachments = []
         sniped_at = datetime.now(timezone.utc)
         snipe_threshold = sniped_at - SNIPE_WINDOW
 
@@ -74,18 +77,21 @@ class Sniper(commands.Cog):
                         sniped_user = cache_entry[KEY_AUTHOR]
                     if cache_entry[KEY_AUTHOR].id == sniped_user.id:
                         sniped_messages.append(cache_entry[KEY_MESSAGE_TEXT])
+                        sniped_attachments += cache_entry[KEY_ATTACHMENTS]
                         message_cache.pop(cache_index)
                         continue  # Continue without incrementing the index because we removed the current entry.
                 cache_index += 1
 
-        await Sniper.send_snipe_response(channel, sniped_user, sniped_messages, sniped_at)
+        await Sniper.send_snipe_response(channel, sniped_user, sniped_messages, sniped_attachments, sniped_at)
 
     @staticmethod
-    async def send_snipe_response(channel, user, messages, timestamp):
+    async def send_snipe_response(channel, user, messages, attachments, timestamp):
         if user and messages:
             embed = create_basic_embed('\n'.join(messages))
             embed.set_author(name=f'{user.name}#{user.discriminator}', icon_url=user.avatar_url)
             embed.timestamp = timestamp
+            if (len(attachments) == 1) and ('image' in attachments[0].content_type):
+                embed.set_image(url=attachments[0].url)
             await channel.send(embed=embed)
         else:
             embed = create_basic_embed(TEXT_SNIPER_FAIL)
