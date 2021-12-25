52640d6d9d057b498a49aded954a3583d00a90bf
Lovegood <75226695+DarthLovegood@users.noreply.github.com>
2021-12-25 15:56:19 -0800

    Some miscellaneous cleanup, including removing clownquest cog. (#19)
    * Some miscellaneous cleanup, including removing clownquest cog.
    
    * Also move nicknames to DEV because it needs more work.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import discord
-import sys
+from discord import Game, Intents
 from discord.ext import commands
-from lib.prefixes import get_prefix
+from lib.prefixes import get_prefix, DEFAULT_PREFIX
 from secrets import BOT_TOKEN_DEV, BOT_TOKEN_LITE, BOT_TOKEN_PROD
+from sys import argv
+
+BOT_STATUS = DEFAULT_PREFIX + 'help'
 
 CONFIG_LITE = 'LITE'
 CONFIG_PROD = 'PROD'
 CONFIG_DEV = 'DEV'
 
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.members = True
 intents.guild_typing = True
 
@@ -29,7 +31,6 @@ def initialize_bot(config):
     bot.load_extension('cogs.audio_player')
     bot.load_extension('cogs.easter_eggs')
     bot.load_extension('cogs.greetings')
-    bot.load_extension('cogs.nicknames')
     bot.load_extension('cogs.rewrite')
     bot.load_extension('cogs.sniper')
 
@@ -37,8 +38,8 @@ def initialize_bot(config):
         return BOT_TOKEN_PROD
 
     # Extensions that should only be loaded for DEV configuration.
-    bot.load_extension('cogs.clownquest')
     bot.load_extension('cogs.events')
+    bot.load_extension('cogs.nicknames')
 
     return BOT_TOKEN_DEV
 
@@ -46,11 +47,11 @@ def initialize_bot(config):
 @bot.event
 async def on_ready():
     print(f'Successfully logged in as: {bot.user}')
-    await bot.change_presence(activity=discord.Game(name='!cb help'))
+    await bot.change_presence(activity=Game(name=BOT_STATUS))
 
 
 if __name__ == '__main__':
-    config_arg = '' if len(sys.argv) != 2 else sys.argv[1].upper()
+    config_arg = '' if len(argv) != 2 else argv[1].upper()
     config_option = config_arg if config_arg in [CONFIG_LITE, CONFIG_PROD, CONFIG_DEV] else CONFIG_PROD
     bot_token = initialize_bot(config_option)
     print(f'[Configuration: {config_option}] Logging in...')
